f16e5565564c6a34972a6df09798bb7a6f573ba8
Motoharu-Ito-for2030 <sketdance2013@outlook.jp>
2021-01-19 19:35:12 +0900

    reqirements deleted

--- pairwork/settings.py
+++ pairwork/settings.py
@@ -129,7 +129,4 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static')
-)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
